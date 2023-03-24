5cab4ad60bfb3315117e7b223bcf30db5207b37e
ayaz ibrahimov1~ <ayaz.ibrahimov.1998@mail.ru>
2023-03-24 14:18:23 +0400

    click button form enable

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+// let mapEvent;
+
 
 if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
@@ -22,7 +25,7 @@ if(navigator.geolocation){
 
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-        var map = L.map('map').setView(coords, 14);
+         map = L.map('map').setView(coords, 14);
 
           L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -30,23 +33,13 @@ if(navigator.geolocation){
 
          L.marker(coords).addTo(map).bindPopup('A pretty CSS3 popup.<br> Easily customizable.').openPopup();
 
-         map.on('click',function(mapEvent){
-            const {lat,lng} = mapEvent.latlng
-             
-            // const data = [lat,lng]
-           
-            L.marker([lat,lng])
-            .addTo(map)
-            .bindPopup(
-                L.popup({
-                maxWidth:250,
-                minWidth:100,
-                autoClose:false,
-                closeOnClick:false,
-                className:'running-popup'
-            }))
-            .setPopupContent('Salam ay qaqa')
-            .openPopup();
+         map.on('click',function(mapE){
+            
+            mapEvent = mapE
+
+            form.classList.remove('hidden')
+            inputDistance.focus() 
+
 
          })
 
@@ -56,6 +49,47 @@ if(navigator.geolocation){
         alert('salala')
     }
 }
+
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+
+    inputElevation.value = inputCadence.value = inputDuration.value = inputDistance.value = ''
+
+    //display marker
+
+    const {lat,lng} = mapEvent.latlng
+                
+    L.marker([lat,lng])
+    .addTo(map)
+    .bindPopup(
+        L.popup({
+        maxWidth:250,
+        minWidth:100,
+        autoClose:false,
+        closeOnClick:false,
+        className:'running-popup'
+    }))
+    .setPopupContent('Salam ay qaqa')
+    .openPopup();
+
+})
+
+
+
+inputType.addEventListener('change',function(){
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+})
+
+
+
+
+
+
+
+
 
 
 
